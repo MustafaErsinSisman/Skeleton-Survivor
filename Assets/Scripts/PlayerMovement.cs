625cc0930bf60d46c1cdcf53c72bd264c5540b74
Mustafa Ersin Şişman <sismanmustafaersin@gmail.com>
2025-11-07 14:25:16 +0300

    joystick kontrolleri eklendi

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5.0f;
+    [Header("Joystick Referansı")]
+    public Joystick joystick;
 
+    [Header("Oyuncu Ayarları")]
+    [SerializeField] private float hareketHizi = 5.0f;
+    
     private CharacterController controller;
+
 
     private void Start()
     {
@@ -14,11 +19,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float yatayInput = joystick.Horizontal;
+        float dikeyInput = joystick.Vertical;
 
-        Vector3 direction = new Vector3(horizontal, 0, vertical);
-       
-        controller.Move(direction * speed * Time.deltaTime);
+        Vector3 horizontalMove = new Vector3(yatayInput, 0, dikeyInput).normalized;
+        
+        if (horizontalMove.magnitude >= 0.1f)
+        {
+            controller.Move(horizontalMove * hareketHizi * Time.deltaTime);
+        }
     }
 }
